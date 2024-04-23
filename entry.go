6bb4f1157a430c0977d09424808d270a0fe1f9cc
Tomas Machalek <tomas@machalek.xyz>
2024-04-23 15:57:42 +0200

    String() and Bool() methods

--- entry.go
+++ entry.go
@@ -13,6 +13,28 @@ type Entry struct {
 	data map[string]any
 }
 
+func (e *Entry) String() string {
+	keys := e.orderedDataKeys()
+	var ans strings.Builder
+	ans.WriteString(fmt.Sprintf("Entry{ts: %s", e.ts))
+	for _, k := range keys {
+		v := e.data[k]
+		switch tv := v.(type) {
+		case float64, float32:
+			ans.WriteString(fmt.Sprintf(", %s: %.3f", k, tv))
+		case int, int64:
+			ans.WriteString(fmt.Sprintf(", %s: %d", k, tv))
+		case bool:
+			ans.WriteString(fmt.Sprintf(", %s: %t", k, tv))
+		default:
+			ans.WriteString(fmt.Sprintf(", %s: %s", k, tv))
+		}
+
+	}
+	ans.WriteString("}")
+	return ans.String()
+}
+
 func (e *Entry) Str(key, value string) *Entry {
 	e.data[key] = value
 	return e
@@ -28,12 +50,22 @@ func (e *Entry) Float(key string, value float64) *Entry {
 	return e
 }
 
-func (e *Entry) ExportForSQL(table, tsCol string) (string, []any) {
+func (e *Entry) Bool(key string, value bool) *Entry {
+	e.data[key] = value
+	return e
+}
+
+func (e *Entry) orderedDataKeys() []string {
 	keys := make([]string, 0, len(e.data)+1) // +1 <= timestamp
 	for k := range e.data {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
+	return keys
+}
+
+func (e *Entry) ExportForSQL(table, tsCol string) (string, []any) {
+	keys := e.orderedDataKeys()
 	var sqlEntries strings.Builder
 	args := make([]any, len(keys)+1)
 
--- entry_test.go
+++ entry_test.go
@@ -29,3 +29,22 @@ func TestMakeEntry(t *testing.T) {
 	assert.Equal(t, "production", args[2])
 	assert.Equal(t, "Kontext", args[3])
 }
+
+func TestString(t *testing.T) {
+	table := NewTableWriter(nil, "foo", "ts")
+	loc, err := time.LoadLocation("Europe/Prague")
+	if err != nil {
+		panic(err)
+	}
+	ts := time.Date(2024, 4, 23, 14, 21, 45, 0, loc)
+	p := table.NewEntry(ts).
+		Str("service", "Kontext").
+		Str("instance", "production").
+		Float("cpu_load", 0.8).
+		Bool("has_error", false)
+	assert.Equal(
+		t,
+		"Entry{ts: 2024-04-23 14:21:45 +0200 CEST, cpu_load: 0.800, "+
+			"has_error: false, instance: production, service: Kontext}",
+		p.String())
+}
